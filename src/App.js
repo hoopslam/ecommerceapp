8c9a425ac74a7875f4d9c3c02532f3fe548b4177
David Cho <hoopslam@gmail.com>
2021-03-09 03:01:42 +0900

    added redux and implemented userReducer and userAction to header component

--- src/App.js
+++ src/App.js
@@ -1,39 +1,32 @@
 import React from "react";
 import "./App.css";
 import { Switch, Route } from "react-router-dom";
+import { connect } from 'react-redux';
 
 import HomePage from "./Pages/HomePage/HomePage";
 import ShopPage from "./Pages/Shop/ShopPage";
 import SignInPage from "./Pages/SignInPage/SignInPage";
 import Header from "./Component/Header/Header";
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
+import { setCurrentUser} from './redux/user/user.actions';
 
 class App extends React.Component {
-	constructor() {
-		super();
-
-		this.state = {
-			currentUser: null,
-		};
-	}
-
 	unsubscribeFromAuth = null;
 
 	componentDidMount() {
+		const {setCurrentUser} = this.props;
 		this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
 			if (userAuth) {
 				const userRef = await createUserProfileDocument(userAuth);
 
 				userRef.onSnapshot(snapShot => {
-					this.setState({
-						currentUser: {
-							id: snapShot.id,
-							...snapShot.data()
-						}
+					setCurrentUser({
+						id: snapShot.id,
+						...snapShot.data()
 					})
 				})
 			}
-			this.setState({currentUser: userAuth}) //set currentUser to null
+			setCurrentUser(userAuth)
 		});
 	}
 
@@ -44,7 +37,7 @@ class App extends React.Component {
 	render() {
 		return (
 			<div>
-				<Header currentUser={this.state.currentUser}/>
+				<Header />
 				<Switch>
 					<Route exact path='/' component={HomePage} />
 					<Route path='/shop' component={ShopPage} />
@@ -55,4 +48,8 @@ class App extends React.Component {
 	}
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+	setCurrentUser: user => dispatch(setCurrentUser(user))
+});
+
+export default connect(null, mapDispatchToProps)(App);
--- src/Component/SignIn/SignIn.jsx
+++ src/Component/SignIn/SignIn.jsx
@@ -56,7 +56,7 @@ export class SignIn extends Component {
 					/>
 					<div className='buttons'>
 						<CustomButton type='submit'> Sign In </CustomButton>
-						<CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+						<CustomButton type="button" onClick={signInWithGoogle} isGoogleSignIn>
 							{" "}
 							Sign In with google{" "}
 						</CustomButton>
